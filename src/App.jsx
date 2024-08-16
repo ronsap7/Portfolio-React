7b02b4687f24ae3fbef26381a7adc275297af236
Rohan Sapate <rhnsapate16@gmail.com>
2024-08-16 19:58:07 +0530

    Project feature along wiht json file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Skills } from "./components/Skills/Skills";
+import {Projects} from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero />
     <About />
     <Skills />
+    <Projects />
     </div>
   );
 }
