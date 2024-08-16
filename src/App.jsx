88725279f03404a279c60f0e0292f2f21dfc40a1
Rohan Sapate <rhnsapate16@gmail.com>
2024-08-16 20:10:29 +0530

    contact-feature and minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Skills } from "./components/Skills/Skills";
 import {Projects} from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <About />
     <Skills />
     <Projects />
+    <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:rhnsapate16@email.com">rhnsapate16@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/rsrohan">linkedin.com/rsrohan</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/ronsap7">github.com/ronsap7</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
